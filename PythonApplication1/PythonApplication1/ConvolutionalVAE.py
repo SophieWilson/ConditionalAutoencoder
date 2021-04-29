d77d3fc27ecd7187bdca23d6c3a880ca3f5178eb
Sophie Wilson <sophie.w783@gmail.com>
2021-04-29 14:36:39 +0100

    This failed. Weird errors are occuring so im going to try another method

--- PythonApplication1/PythonApplication1/ConvolutionalVAE.py
+++ PythonApplication1/PythonApplication1/ConvolutionalVAE.py
@@ -2,6 +2,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+from keras import backend as K
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
@@ -15,73 +17,71 @@ if gpus:
         print(e)
 
 
-epochs = 50
-origin_dim = 28 * 28
+epochs = 5
+#origin_dim = 28 * 28
 batch_size = 128
-intermediate_dim = 64
+#intermediate_dim = 64
 latent_dim = 2 
-#encoder_inputs = keras.Input(shape=(origin_dim,))
 
 # Make a sampling layer, this maps the MNIST digit to latent-space triplet (z_mean, z_log_var, z), this is how the bottleneck is displayed. 
-from keras import backend as K
-def sampling(args):
+
+class sampling(layers.Layer):
     """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
-    z_mean, z_log_sigma = args
-    #batch = tf.shape(z_mean)[0]
-    #dim = tf.shape(z_mean)[1]
-    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean = 0., stddev=0.1)
-    return z_mean + K.exp(z_log_sigma) * epsilon
+    def call(self, inputs):
+        z_mean, z_log_sigma = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean = 0., stddev=0.1)
+        return z_mean + K.exp(z_log_sigma) * epsilon
 
 
 ## Make the encoder, input > cov2D > flatten > dense (pretty sure this is doing nothing right now)
-# convolution layers
-encoder_input = keras.Input(shape=(28, 28, 1))
-x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_input)
-x = layers.MaxPooling2D((2, 2), padding='same')(x)
-x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = layers.MaxPooling2D((2, 2), padding='same')(x)
-encoded = layers.Conv2D(8, (3, 3), activation='relu', padding='same', name = 'encoded')(x)
-############## This might be deleted below
-h = layers.Dense(intermediate_dim, activation='relu')(x)
-z_mean = layers.Dense(latent_dim, name="z_mean")(h)
-z_log_sigma = layers.Dense(latent_dim, name="z_log_sigma")(h)
-## save shape before flattening for decoder
-from keras import backend
-shape_before_flattening = keras.backend.int_shape(x)
-##############
-## flatten and dense
-x = layers.Flatten()(encoded)
-x = layers.Dense(16, activation="relu")(x)
 # outputs, as this is variational you have two outputs, the mean and the sigma of the latent dimension, so it takes a sample from this distribtion to run through back propagation. As you cant back propagation from a sample distribution epsilon is added to z to allow it to be run through the decoder. This is what the sampling funciton does. (WHY RUN THROUGH LAMBDA)
-z = layers.Lambda(sampling)([z_mean, z_log_sigma])
 
-# initiating the encoder, it ouputs the latent dim dimensions
-encoder = keras.Model(encoder_input, [z_mean, z_log_sigma, z], name="encoder")
+encoder_inputs = keras.Input(shape=(28, 28, 1))
+x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
+x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+shape_before_flattening = K.int_shape(x)
+x = layers.Flatten()(x)
+x = layers.Dense(16, activation="relu")(x)
+#x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_input)
+#x = layers.MaxPooling2D((2, 2), padding='same')(x)
+#x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+#x = layers.MaxPooling2D((2, 2), padding='same')(x)
+#x = layers.Conv2D(8, (3, 3), activation='relu', padding='same', name = 'encoded')(x)
+z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+z_log_sigma = layers.Dense(latent_dim, name="z_log_sigma")(x)
+z = sampling()([z_mean, z_log_sigma])
+encoder = keras.Model(encoder_inputs, [z_mean, z_log_sigma, z], name="encoder")
 encoder.summary()
 
 
-### Make the decoder, takes the latent
-decoder_inputs = keras.Input(shape=(16,))
+decoder_inputs = keras.Input(shape=(latent_dim,))
 x = layers.Dense(7 * 7 * 64, activation="relu")(decoder_inputs)
 x = layers.Reshape((7, 7, 64))(x)
 x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
 x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
-#x = layers.Dense(intermediate_dim, activation='relu')(latent_inputs)
-decoder_outputs = layers.Conv2DTranspose(1, (3, 3), activation='sigmoid', padding='same')(x) # 28 x 28 x 1
+decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
 decoder = keras.Model(decoder_inputs, decoder_outputs, name="decoder")
 decoder.summary()
+#decoder_inputs = keras.Input(shape=(16,))
+#self.dense1 = layers.Dense(7 * 7 * 64, activation="relu")
+#self.reshape1 = layers.Reshape((7, 7, 64))
+#self.cov1T = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")
+#self.cov2T = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")
+##x = layers.Dense(intermediate_dim, activation='relu')(latent_inputs)
+#self.outdec = layers.Conv2DTranspose(1, (3, 3), activation='sigmoid', padding='same') # 28 x 28 x 1
 
-# instantiate the VAE model (simplified)
-outputs = decoder(encoder(encoder_input)[2])
-vae = keras.Model(encoder_input, outputs, name='vae')
+# Make the VAE class
 
-#######################################################################################
-## Define the VAE as a model with custom train_step
 #class VAE(keras.Model):
+#    ''' use a custom loss function, this includes a KL divergence regularisation term which ensures that z is close to normal (0 mean, 1 sd) '''
 #    def __init__(self, encoder, decoder, **kwargs):
 #        super(VAE, self).__init__(**kwargs)
 #        self.encoder = encoder
 #        self.decoder = decoder
+
 #        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
 #        self.reconstruction_loss_tracker = keras.metrics.Mean(
 #            name="reconstruction_loss"
@@ -96,52 +96,83 @@ vae = keras.Model(encoder_input, outputs, name='vae')
 #            self.kl_loss_tracker,
 #        ]
 
-#    def train_step(self, data):
-#        with tf.GradientTape() as tape:
-#            z_mean, z_log_var, z = self.encoder(data)
-#            reconstruction = self.decoder(z)
-#            reconstruction_loss = tf.reduce_mean(
-#                tf.reduce_sum(
-#                    keras.losses.binary_crossentropy(data, reconstruction), axis=(1, 2)
-#                )
-#            )
-#            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
-#            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
-#            total_loss = reconstruction_loss + kl_loss
-#        grads = tape.gradient(total_loss, self.trainable_weights)
-#        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
-#        self.total_loss_tracker.update_state(total_loss)
-#        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
-#        self.kl_loss_tracker.update_state(kl_loss)
-#        return {
-#            "loss": self.total_loss_tracker.result(),
-#            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
-#            "kl_loss": self.kl_loss_tracker.result(),
-#        }
-#########################################################################################
+    #def train_step(self, x_train):
+    #    with tf.GradientTape() as tape:
+    #        z_mean, z_log_var, z = self.encoder(x_train)
+    #        reconstruction = self.decoder(z)
+    #        reconstruction_loss = tf.reduce_mean(
+    #            tf.reduce_sum(
+    #                keras.losses.binary_crossentropy(x_train, reconstruction), axis=(1, 2)
+    #            )
+    #        )
+    #        kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+    #        kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+    #        total_loss = reconstruction_loss + kl_loss
+    #    grads = tape.gradient(total_loss, self.trainable_weights)
+    #    self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+    #    self.total_loss_tracker.update_state(total_loss)
+    #    self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+    #    self.kl_loss_tracker.update_state(kl_loss)
+    #    return {
+    #        "loss": self.total_loss_tracker.result(),
+    #        "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+    #        "kl_loss": self.kl_loss_tracker.result(),
+    #    }
 
-# use a custom loss function, this includes a KL divergence regularisation term which ensures that z is close to normal (0 mean, 1 sd)
-reconstruction_loss = keras.losses.binary_crossentropy(encoder_input, outputs)
-reconstruction_loss *= origin_dim
-kl_loss = 1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma)
-kl_loss = K.sum(kl_loss, axis=-1)
-kl_loss *= -0.5
-vae_loss = K.mean(reconstruction_loss + kl_loss)
-vae.add_loss(vae_loss)
-vae.compile(optimizer=keras.optimizers.Adam())
+
 
 # train VAE on MNIST
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+
+outputs = decoder(encoder(encoder_inputs)[2])
+vae = keras.Model(encoder_inputs, outputs, name='vae')
+vae.compile(optimizer=keras.optimizers.Adam())
+history = vae.fit(x_train, x_train, epochs=epochs, batch_size=batch_size)
+# .fit function returns history object with loss metrics
+print(history.history.keys())
 
 # fit the data
 vae.fit(x_train, x_train, epochs=epochs, batch_size=batch_size, validation_data = (x_test, x_test))
 
+#encoder.compile(optimizer=keras.optimizers.Adam())
+#encoder.fit(x_train, x_train, epochs=epochs, batch_size=batch_size, validation_data = (x_test, x_test))
+#encoder.predict(x_train)
+
+# fit the data
+
+
+
+
+
+# loss plot
+def lossplot(history):
+    loss_values = history.history['loss']
+    reconstruction_loss = history.history['reconstruction_loss']
+    kl_loss = history.history['kl_loss']
+    epochs = range(1, len(loss_values)+1)
+    plt.plot(epochs, loss_values, label='Training Loss')
+    plt.plot(epochs, reconstruction_loss, label='Reconstruction Loss')
+    #plt.plot(epochs, kl_loss, label='KL Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    #plt.show()
+
+lossplot(history)
+#loss_values = history.history['accuracy']
+#epochs = range(1, len(loss_values)+1)
+#plt.plot(epochs, loss_values, label='Training Accuracy')
+#plt.xlabel('Epochs')
+#plt.ylabel('Accuracy')
+#plt.legend()
+#plt.show()
+
 # Display how the latent space clusters the digit classes
-import matplotlib.pyplot as plt
+
 def plot_clusters(encoder, data, labels, batch_size):
     x_test_encoded, _, _ = encoder.predict(x_test, batch_size=batch_size)
     plt.figure(figsize=(6, 6))
